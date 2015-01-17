891f90d705b251431f7c9e0e24e47cad7221d1f9
David Rea <drea@appliedlogix.com>
2015-01-17 15:29:44 -0500

    Templatize notebook list with demo helper and scaffolded data

---
+++ client/templates/notebook_list.js
@@ -0,0 +1,21 @@
+var notebookData = [
+
+{
+	name: "FN - Kraft - 1",
+	startDate: "2013-03-29",
+	endDate: "2013-12-31"
+},
+
+{
+	name: "FN - Drink Local - Amber Ale",
+	startDate: "2014-01-01",
+	endDate: "2014-04-09"
+},
+
+]
+
+Template.notebookList.helpers( {
+
+	notebooks: notebookData
+
+} )
