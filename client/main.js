f24b01aa8ba0bbcfa1f511aecd64ea7fbfa97fbd
David Rea <drea@appliedlogix.com>
2015-01-25 22:14:06 -0500

    Auto-collapse bootstrap menu when a link is clicked

---
+++ client/main.js
@@ -0,0 +1,5 @@
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
