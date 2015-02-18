cc64a8c95e62b096f96edef540daf341afe52a3c
David Rea <drea@appliedlogix.com>
2015-02-18 08:11:40 -0500

    Add user-related hooks for Google Analytics

---
+++ client/hooks.js
@@ -0,0 +1,17 @@
+Hooks.init();
+
+Hooks.onLoggedIn = function() {
+	ga('send', 'event', 'user', 'logged-in');
+	Router.go('notebookList');
+}
+
+Hooks.onLoggedOut = function(userid) {
+	check(userid, String);
+	ga('send', 'event', 'user', 'logged-out');
+	Router.go('welcome');
+}
+
+Hooks.onCreateUser = function(userid) {
+	check(userid, String);
+	ga('send', 'event', 'user', 'logged-in');
+}
