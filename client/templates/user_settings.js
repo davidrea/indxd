0a476b7c664a20e3eb67f3767da6ea08e8a1c2b0
David Rea <drea@appliedlogix.com>
2015-04-15 23:22:48 -0400

    New template and nav for personal settings page, helper stubs

---
+++ client/templates/user_settings.js
@@ -0,0 +1,23 @@
+Template.userSettings.helpers({
+
+	backupsEnabled: function() {
+		return "Off";
+	},
+
+	backupsOn: function() {
+		return false;
+	},
+
+	backpsWhen: function() {
+		return "Every Week";
+	},
+
+	emailPreference: function() {
+		return "Off";
+	},
+
+	emailMember: function() {
+		return false;
+	}
+
+});
