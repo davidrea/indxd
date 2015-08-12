2ceb5746a8194e480eabad6eb4928653c464fe51
David Rea <drea@appliedlogix.com>
2015-08-12 00:23:23 -0400

    Collapse duplicate topics in topic list

--- client/templates/topic_list.js
+++ client/templates/topic_list.js
@@ -10,26 +10,52 @@ Template.topicList.helpers({
 			return o;
 		}, {});
 
-		_.each(letters, function(element, index, list) {
+		_.each(letters, function(element, letterindex, list) {
 
-			var letter = list[index].letter.toLowerCase();
+			var letter = list[letterindex].letter.toLowerCase();
 			var regex = new RegExp('^' + letter, 'i');
 
 			// Determine if there are topics for this letter
-			var letterTopics = Topics.find({
-				topic: regex
-			}, {sort: {topic: 1}});
+			var letterTopicNames = _.uniq(Topics.find(
+				{topic: regex},
+				{
+					sort: {topic: 1},
+					fields: {topic: true}
+				}).fetch().map(function(x) {
+					return x.topic;
+				}), true);
 
-			var count = letterTopics.count();
+			var count = letterTopicNames.length;
+
+			// Create list of topics
 
 			if(count > 0) {
-				letters[index].hasTopics = true;
-			}
+				letters[letterindex].hasTopics = true;
+				letters[letterindex].topicsForLetter = [];
 
-			// Populate the list of topics
-			letters[index].topicsForLetter = letterTopics;
+				// Get topic content for each name
+				_.each(letterTopicNames, function(topicName, index, list) {
+					var topic = {};
+					topic.topic = topicName;
+					topic.bookList = [];
+					topic.bookCount = 0;
+					topic.multipleBooks = 0;
+					var topics = Topics.find({topic: topicName});
+					if(topics.count() > 1) { topic.multipleBooks = 1; }
+					_.each(topics.fetch(), function(thetopic, index, list) {
+						topic.bookCount += 1;
+						var book = {};
+						book.referringTopic = topicName;
+						book.bookName = thetopic.bookName;
+						book.bookId = thetopic.bookId;
+						book.bookPage = thetopic.page;
+						topic.bookList.push(book);
+					});
+					letters[letterindex].topicsForLetter.push(topic);
+				});
+			}
 		});
-		
+
 		return letters;
 	},
 
